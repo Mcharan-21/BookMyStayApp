d6c8563eb2604073d7043991dc67d4e81622cf23
CHARAN MOPADA <mopadacharan@gmail.com>
2026-03-25 14:39:37 +0530

    UC2 Basic Room Types and Static Availability

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,11 +1,60 @@
+abstract class Room {
+
+    private String type;
+    private int beds;
+    private double price;
+
+    public Room(String type, int beds, double price) {
+        this.type = type;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getBeds() {
+        return beds;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
+
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super("Single Room", 1, 1000);
+    }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super("Double Room", 2, 2000);
+    }
+}
+
+class SuiteRoom extends Room {
+    public SuiteRoom() {
+        super("Suite Room", 3, 5000);
+    }
+}
+
 public class BookMyStayApp {
 
     public static void main(String[] args) {
 
-        System.out.println("   Welcome to Hotel Booking System  ");
-        System.out.println("   Version: 1.0                     ");
+        Room single = new SingleRoom();
+        Room dual = new DoubleRoom();
+        Room suite = new SuiteRoom();
 
-        System.out.println("Application started successfully.");
-        System.out.println("Application terminating...");
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        System.out.println(single.getType() + " | Beds: " + single.getBeds() + " | Price: " + single.getPrice() + " | Available: " + singleAvailable);
+        System.out.println(dual.getType() + " | Beds: " + dual.getBeds() + " | Price: " + dual.getPrice() + " | Available: " + doubleAvailable);
+        System.out.println(suite.getType() + " | Beds: " + suite.getBeds() + " | Price: " + suite.getPrice() + " | Available: " + suiteAvailable);
     }
 }
