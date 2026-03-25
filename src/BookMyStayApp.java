5e9b0bb26fa12e9f4441969a5bc26cd4f8061d42
CHARAN MOPADA <mopadacharan@gmail.com>
2026-03-25 14:22:26 +0530

    UC1: Application startup with welcome message and version info

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,2 +1,11 @@
 public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("   Welcome to Hotel Booking System  ");
+        System.out.println("   Version: 1.0                     ");
+
+        System.out.println("Application started successfully.");
+        System.out.println("Application terminating...");
+    }
 }
