6228eb2f1f0aeb6905720affc7141ef17e7f2e64
CHARAN MOPADA <mopadacharan@gmail.com>
2026-03-25 15:09:45 +0530

    UC12 Data persistence using serialization and file handling

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,98 +1,104 @@
+import java.io.*;
 import java.util.*;
 
-class RoomInventory {
+class RoomInventory implements Serializable {
 
     private HashMap<String, Integer> inventory;
 
     public RoomInventory() {
         inventory = new HashMap<>();
         inventory.put("Single Room", 2);
+        inventory.put("Double Room", 1);
     }
 
-    public synchronized boolean bookRoom(String type) {
+    public HashMap<String, Integer> getInventory() {
+        return inventory;
+    }
 
-        int count = inventory.getOrDefault(type, 0);
+    public void setInventory(HashMap<String, Integer> inventory) {
+        this.inventory = inventory;
+    }
 
-        if (count > 0) {
-            inventory.put(type, count - 1);
-            return true;
+    public void display() {
+        System.out.println("Inventory:");
+        for (String key : inventory.keySet()) {
+            System.out.println(key + " -> " + inventory.get(key));
         }
-
-        return false;
     }
 }
 
-class Reservation {
+class Reservation implements Serializable {
 
+    private String reservationId;
     private String guestName;
-    private String roomType;
 
-    public Reservation(String guestName, String roomType) {
+    public Reservation(String reservationId, String guestName) {
+        this.reservationId = reservationId;
         this.guestName = guestName;
-        this.roomType = roomType;
+    }
+
+    public String getReservationId() {
+        return reservationId;
     }
 
     public String getGuestName() {
         return guestName;
     }
+}
 
-    public String getRoomType() {
-        return roomType;
+class BookingHistory implements Serializable {
+
+    private List<Reservation> history;
+
+    public BookingHistory() {
+        history = new ArrayList<>();
+    }
+
+    public void add(Reservation r) {
+        history.add(r);
+    }
+
+    public List<Reservation> getAll() {
+        return history;
+    }
+
+    public void display() {
+        System.out.println("Booking History:");
+        for (Reservation r : history) {
+            System.out.println(r.getReservationId() + " | " + r.getGuestName());
+        }
     }
 }
 
-class BookingQueue {
+class PersistenceService {
 
-    private Queue<Reservation> queue;
+    private static final String FILE_NAME = "data.ser";
 
-    public BookingQueue() {
-        queue = new LinkedList<>();
+    public void save(RoomInventory inventory, BookingHistory history) {
+
+        try {
+            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(FILE_NAME));
+            out.writeObject(inventory);
+            out.writeObject(history);
+            out.close();
+            System.out.println("Data saved successfully");
+        } catch (Exception e) {
+            System.out.println("Error saving data");
+        }
     }
 
-    public synchronized void add(Reservation r) {
-        queue.add(r);
-    }
+    public Object[] load() {
 
-    public synchronized Reservation get() {
-        return queue.poll();
-    }
-}
-
-class BookingProcessor extends Thread {
-
-    private BookingQueue queue;
-    private RoomInventory inventory;
-
-    public BookingProcessor(BookingQueue queue, RoomInventory inventory) {
-        this.queue = queue;
-        this.inventory = inventory;
-    }
-
-    public void run() {
-
-        while (true) {
-
-            Reservation r;
-
-            synchronized (queue) {
-                r = queue.get();
-            }
-
-            if (r == null) break;
-
-            boolean success;
-
-            synchronized (inventory) {
-                success = inventory.bookRoom(r.getRoomType());
-            }
-
-            if (success) {
-                System.out.println(Thread.currentThread().getName() +
-                        " confirmed booking for " + r.getGuestName());
-            } else {
-                System.out.println(Thread.currentThread().getName() +
-                        " failed booking for " + r.getGuestName());
-            }
+        try {
+            ObjectInputStream in = new ObjectInputStream(new FileInputStream(FILE_NAME));
+            RoomInventory inventory = (RoomInventory) in.readObject();
+            BookingHistory history = (BookingHistory) in.readObject();
+            in.close();
+            System.out.println("Data loaded successfully");
+            return new Object[]{inventory, history};
+        } catch (Exception e) {
+            System.out.println("No previous data found, starting fresh");
+            return null;
         }
     }
 }
@@ -101,21 +107,27 @@ public class BookMyStayApp {
 
     public static void main(String[] args) {
 
-        RoomInventory inventory = new RoomInventory();
-        BookingQueue queue = new BookingQueue();
+        PersistenceService service = new PersistenceService();
 
-        queue.add(new Reservation("Charan", "Single Room"));
-        queue.add(new Reservation("Rahul", "Single Room"));
-        queue.add(new Reservation("Anjali", "Single Room"));
-        queue.add(new Reservation("Kiran", "Single Room"));
+        Object[] data = service.load();
 
-        BookingProcessor t1 = new BookingProcessor(queue, inventory);
-        BookingProcessor t2 = new BookingProcessor(queue, inventory);
+        RoomInventory inventory;
+        BookingHistory history;
 
-        t1.setName("Thread-1");
-        t2.setName("Thread-2");
+        if (data != null) {
+            inventory = (RoomInventory) data[0];
+            history = (BookingHistory) data[1];
+        } else {
+            inventory = new RoomInventory();
+            history = new BookingHistory();
+        }
 
-        t1.start();
-        t2.start();
+        history.add(new Reservation("RES101", "Charan"));
+        history.add(new Reservation("RES102", "Rahul"));
+
+        inventory.display();
+        history.display();
+
+        service.save(inventory, history);
     }
 }
